8b12bd129a7cf67b53967037f16dbc3babb74554
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-04-28 13:21:29 +0200

    Add basic check functions

---
+++ src/Isbn/Check.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Isbn;
+
+class Check {
+
+    public static function isbn($isbn)
+    {
+        if (strlen($isbn) == 13)
+            return Check::isbn13($isbn);
+        if (strlen($isbn) == 10)
+            return Check::isbn10($isbn);
+        return false;
+    }
+
+    public static function isbn10($isbn)
+    {
+        if (strlen($isbn) != 10)
+            return false;
+        $check = 0;
+        for ($i = 0; $i < 10; $i++)
+            if ($isbn[$i] == "X")
+                $check += 10 * intval(10 - $i);
+            else
+                $check += intval($isbn[$i]) * intval(10 - $i);
+        return $check % 11 == 0;
+    }
+
+    public static function isbn13($isbn)
+    {
+        if (strlen($isbn) != 13)
+            return false;
+        $check = 0;
+        for ($i = 0; $i < 13; $i+=2)
+            $check += substr($n, $i, 1);
+        for ($i = 1; $i < 12; $i+=2)
+            $check += 3 * substr($n, $i, 1);
+        return $check % 10 == 0;
+    }
+}
