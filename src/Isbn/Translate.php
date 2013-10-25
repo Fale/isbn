ea5e144ac1257b1664a27fd85ba952f96284adc6
linkkingjay <linkkingjay@gmail.com>
2013-10-26 00:56:01 +0800

    fix Translate bugs
    when translating isbn with hyphens

--- src/Isbn/Translate.php
+++ src/Isbn/Translate.php
@@ -6,14 +6,20 @@ class Translate {
 
     public static function to10($isbn)
     {
-        $isbn = substr($isbn, 3, -1);
+        if (strlen($isbn) > 13)
+            $isbn = substr($isbn, 4, -1);
+        else
+            $isbn = substr($isbn, 3, -1);
         return $isbn . CheckDigit::make($isbn);
     }
 
     public static function to13($isbn)
     {
         $isbn = substr($isbn, 0, -1);
-        $isbn = "978" . $isbn;
+        if (strlen($isbn) > 9)
+            $isbn = "978-" . $isbn;
+        else
+            $isbn = "978" . $isbn;
         return $isbn . CheckDigit::make($isbn);
     }
 
